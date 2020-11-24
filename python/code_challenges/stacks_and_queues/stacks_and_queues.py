3074b6dcf07a1545fddd1a53e8d1fe0a517da3a7
Ryan Pilon <ryanpilon1995@gmail.com>
2020-11-24 09:49:24 -0800

    finished cc 10

--- python/code_challenges/stacks_and_queues/stacks_and_queues.py
+++ python/code_challenges/stacks_and_queues/stacks_and_queues.py
@@ -48,24 +48,49 @@ class Stack:
 
 class Queue:
 
-    def __init__(self, front = None):
+    def __init__(self):
 
-        self.front = front
+        self.front = None
+        self.back = None
+        self.size = 0
 
     def enqueue(self, value):
 
         node = Node(value)
-        front.next = node
-        self.rear = node
+        if not self.front:
+            self.front = node
+            self.back = node
+            self.size += 1
+            return node
+        if value is None:
+            raise InvalidOperationError('Node value cannot be none')
+        self.back.next = node
+        self.back = node
+        self.size += 1
+        if self.size == 1:
+            self.front = node
+        return node
 
 
     def dequeue(self):
-        pass
+        if self.front == None:
+            raise InvalidOperationError('Cannot dequeue from empty queue')
+        current_node = self.front
+        if self.front:
+            self.front = self.front.next
+            self.size -= 1
+        return str(current_node.value)
 
 
     def peek(self):
-        pass
+        if self.front == None:
+            raise InvalidOperationError("Method not allowed on empty collection")
+        return str(self.front.value)
 
 
-    def isEmpty(self):
-        pass
+    def is_empty(self):
+        if not self.front:
+           return True
+        else:
+            return False
+
